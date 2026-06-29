2541db434cf51307cebccc8cf783714238fa3b23
unknown <Administrateur@92SE25-8D068P2.ad.campus-eni.fr>
2026-06-29 17:14:47 +0200

    librairie Machine Learning

---
+++ demo.py
@@ -0,0 +1,14 @@
+print('Hello')
+
+nom_variable = 4
+print(nom_variable)
+
+
+print("Table de multiplications")
+nb = int(input("enter a nombre: "))
+
+i = 0
+while i <  10:
+    i += 1;
+    print(i, "x", nb, "=", i * nb)
+
